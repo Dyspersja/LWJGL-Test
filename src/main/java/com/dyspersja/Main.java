4d023a6536734fde7f16a7c966e207e159f96a05
Dyspersja <dyspersjamusic@gmail.com>
2024-02-22 15:13:00 +0100

    Updated main to use created engine class

--- src/main/java/com/dyspersja/Main.java
+++ src/main/java/com/dyspersja/Main.java
@@ -3,11 +3,8 @@ package com.dyspersja;
 public class Main {
     public static void main(String[] args) {
         Display window = new Display("App Title",400,300);
-        window.create();
+        Engine engine = new Engine(window);
 
-        while (!window.shouldClose()) {
-            window.update();
-        }
-        window.destroy();
+        engine.start();
     }
 }
