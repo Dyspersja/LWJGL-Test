7bcfed0262b71e7c2a64b8a8c547320bee166b1d
Dyspersja <dyspersjamusic@gmail.com>
2024-02-22 15:09:02 +0100

    Created engine loop

--- src/main/java/com/dyspersja/Engine.java
+++ src/main/java/com/dyspersja/Engine.java
@@ -4,19 +4,53 @@ public class Engine {
 
     private final Display window;
 
+    private float targetFPS = 1000;
+    private boolean running;
+
     public Engine(Display window) {
         this.window = window;
     }
 
-    private void init() {
+    public void start() {
         window.create();
+        run();
+        cleanup();
     }
 
-    public void render() {
+    private void run() {
+        running = true;
+
+        double frameTime = 1.0f / targetFPS;
+        long lastTime = System.nanoTime();
+        double deltaTime = 0;
+
+        while (running) {
+            boolean render = false;
+            long currentTime = System.nanoTime();
+            deltaTime += (currentTime - lastTime) / 1e9;
+            lastTime = currentTime;
+
+            while (deltaTime > frameTime) {
+                render = true;
+                deltaTime -= frameTime;
+
+                if (window.shouldClose()) stop();
+            }
+
+            if (render) render();
+        }
+    }
+
+    private void stop() {
+        if (!running) return;
+        running = false;
+    }
+
+    private void render() {
         window.update();
     }
 
-    public void cleanup() {
+    private void cleanup() {
         window.destroy();
     }
 
